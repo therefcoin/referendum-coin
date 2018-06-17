89d281f35fb94073a24bbd54ca1f7f88cc79c6d4
therefcoin <39701259+therefcoin@users.noreply.github.com>
2018-06-17 20:20:33 +1000

    Update referendum.sol

--- referendum.sol
+++ referendum.sol
@@ -4,17 +4,18 @@ contract ReferendumCoin {
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
+    uint8 public decimals = 2;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
+    uint public referendum_start_date;
+    uint public referendum_end_date;
     struct Voter {
         uint weight;
         bool voted;
         uint8 vote;
         address delegate;
         address wallet;
-        uint balanceOf;
         bool coin_received;
     }
     struct Proposal {
@@ -29,15 +30,22 @@ contract ReferendumCoin {
 
     address chairperson;
     mapping(address => Voter) voters;
+    mapping(address => uint) balances;
     Proposal[] proposals;
 
 
     /// Create a new ballot with $(_numProposals) different proposals.
-    function ReferendumCoin(uint8 _numProposals) public {
+    constructor(uint8 _numProposals,string _name,string _symbol,uint256 _totalSupply
+                            ,uint _start,uint _end) public {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
-        voters[chairperson].balanceOf = totalSupply;
+        totalSupply = _totalSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        name = _name;
+        symbol = _symbol;
+        balances[chairperson] = totalSupply;
         proposals.length = _numProposals;
+        referendum_start_date = _start;
+        referendum_end_date = _end;
     }
 
     /**
@@ -47,18 +55,18 @@ contract ReferendumCoin {
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
         // Check if the sender has enough
-        require(voters[_from].balanceOf >= _value);
+        require(balances[_from] >= _value);
         // Check for overflows
-        require(voters[_to].balanceOf + _value >= voters[_to].balanceOf);
+        require(balances[_to] + _value >= balances[_to]);
         // Save this for an assertion in the future
-        uint previousBalances = voters[_from].balanceOf + voters[_to].balanceOf;
+        uint previousBalances = balances[_from] + balances[_to];
         // Subtract from the sender
-        voters[_from].balanceOf -= _value;
+        balances[_from] -= _value;
         // Add the same to the recipient
-        voters[_to].balanceOf += _value;
+        balances[_to] += _value;
         emit Transfer(_from, _to, _value);
         // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(voters[_from].balanceOf + voters[_to].balanceOf == previousBalances);
+        assert(balances[_from] + balances[_to] == previousBalances);
     }
 
     /**
@@ -102,12 +110,21 @@ contract ReferendumCoin {
     function vote(uint8 toProposal) public {
         Voter storage sender = voters[msg.sender];
         if (sender.voted || toProposal >= proposals.length) return;
+        if (now  < referendum_start_date || now > referendum_end_date ) return;
         sender.voted = true;
         sender.vote = toProposal;
         proposals[toProposal].voteCount += sender.weight;
         if (msg.sender != chairperson ) {
             _transfer(chairperson,msg.sender,1);
         }
+    }
+
+    /// Give a single vote to proposal $(toProposal).
+    function extend_referendum_date(uint new_date) public {
+        if( chairperson != msg.sender) return;
+        if(new_date <= referendum_end_date) return;//invalid date
+        referendum_end_date = new_date;
+        
     }
 
     function winningProposal() public constant returns (uint8 _winningProposal) {
@@ -118,7 +135,4 @@ contract ReferendumCoin {
                 _winningProposal = prop;
             }
     }
-
-
-    
 }
